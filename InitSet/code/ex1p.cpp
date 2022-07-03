a1968f661b3aa45f526774a001aaa75d11a2c7c9
jrubianom <jrubianom@unal.edu.co>
2022-07-03 03:00:35 -0500

    Functions that read and write mesh and init vel

--- InitSet/code/ex1p.cpp
+++ InitSet/code/ex1p.cpp
@@ -15,17 +15,18 @@ struct Config
     int serial_refinements = 1;
     int parallel_refinements = 0;
     int order = 2;
+
+  
+  //We have to include this two variables
     int pid = -1;
-    bool restart = false;
+    bool restart = true;
+
 } Parameters;
 
 
 void SaveState(ParMesh *pmesh,ParGridFunction *Velocity);
 ParGridFunction InitState(ParMesh *pmesh,ParFiniteElementSpace &vfes);
 ParMesh InitMesh();
-
-double radius = 2;
-double vel = 1;
 
 void Initial_Velocity(const Vector &x, Vector &u){
   u(0) = x(0)*x(0);
@@ -35,7 +36,7 @@ void Initial_Velocity(const Vector &x, Vector &u){
 
 int main(int argc, char *argv[])
 {
-   // 1. Initialize MPI and HYPRE.
+   // Initialize MPI and HYPRE.
    Mpi::Init();
    int num_procs = Mpi::WorldSize();
    int myid = Mpi::WorldRank();
@@ -43,54 +44,62 @@ int main(int argc, char *argv[])
    Hypre::Init();
 
    
-   // 2. Parse command-line options.
+   // Parse command-line options.
    int order = 2;
    bool static_cond = false;
    bool pa = false;
    const char *device_config = "cpu";
    bool visualization = true;
    bool algebraic_ceed = false;
-
-   // 3. Enable hardware devices such as GPUs, and programming models such as
-   //    CUDA, OCCA, RAJA and OpenMP based on command line options.
    Device device(device_config);
    if (Parameters.pid == 0) { device.Print(); }
+
+
+   //Parallel MESH
    ParMesh pmesh_var = InitMesh();
    ParMesh *pmesh = &pmesh_var;
-
-   return 0;
    H1_FECollection vfec = H1_FECollection(Parameters.order, pmesh->Dimension());
    ParFiniteElementSpace vfes = ParFiniteElementSpace(pmesh, &vfec, pmesh->Dimension());
-   if (Parameters.pid == 0)
-   {
-     std::cout << "Number of finite element unknowns: "  << std::endl;
-   }
-   // 10. Define the solution vector x as a parallel finite element grid
-   //     function corresponding to fespace. Initialize x with initial guess of
-   //     zero, which satisfies the boundary conditions.
+   
+   
+   //Define velocity coeff in order to checkcomparison the error with the coeff readed from file (when paramaters.restart = false)
+   VectorFunctionCoefficient u_check(pmesh->Dimension(), Initial_Velocity);
+   //Create or read the initial velocity
    ParGridFunction u_var = InitState(pmesh,vfes);
    ParGridFunction *u = &u_var;
-   
+
+   //get error
+   double error = u_var.ComputeL2Error(u_check);
+   //print error 
+   if(myid == 0){
+     std::cout << "Restart Flag:\t" << Parameters.restart << "\n";
+     std::cout << "Error\t" << error << "\n";
+   }
+
+   //save the state if restart = true 
    SaveState(pmesh,u);
+   
    return 0;
 }
 
 //Print the final results
 void SaveState(ParMesh *pmesh,ParGridFunction *Velocity){
+
+  if(Parameters.restart){
     //Save final state
     std::ofstream out;
     std::ostringstream oss;
-    oss << std::setw(10) << std::setfill('0') << Parameters.pid;
+    oss << std::setw(5) << std::setfill('0') << Parameters.pid;
     std::string n_mesh = "Initial/pmesh_"+oss.str()+".msh";
-    std::string n_velocity = "Initial/velocity"+oss.str()+".gf";
+    const char *n_velocity = "Initial/velocity";
     
     out.open(n_mesh.c_str(),std::ios::out);
+    out.precision(16);
     pmesh->ParPrint(out);
     out.close();
     
-    out.open(n_velocity.c_str(),std::ios::out);
-    Velocity->Save(out);
-    out.close();
+    Velocity->Save(n_velocity);
+  }
 }
 
 ParGridFunction InitState(ParMesh *pmesh,ParFiniteElementSpace &vfes){
@@ -102,8 +111,8 @@ ParGridFunction InitState(ParMesh *pmesh,ParFiniteElementSpace &vfes){
   else{  
     std::ifstream in;
     std::ostringstream oss;
-    oss << std::setw(10) << std::setfill('0') << Parameters.pid;
-    std::string n_velocity = "Initial/velocity"+oss.str()+".gf";
+    oss << Parameters.pid;
+    std::string n_velocity = "Initial/velocity.00000"+std::to_string(Parameters.pid);
     in.open(n_velocity.c_str(),std::ios::in);
     u = ParGridFunction(pmesh, in);
     in.close();
@@ -112,7 +121,8 @@ ParGridFunction InitState(ParMesh *pmesh,ParFiniteElementSpace &vfes){
 }
 
 ParMesh InitMesh(){
-  ParMesh pmesh;
+
+  //if there are not files yet
   if(Parameters.restart){
     Mesh mesh = Mesh::MakeCartesian3D(2*Parameters.n, Parameters.n, Parameters.n, Element::QUADRILATERAL, Parameters.Lx, Parameters.Ly, Parameters.Lz);
     mesh.EnsureNodes();
@@ -122,20 +132,19 @@ ParMesh InitMesh(){
     for (int i = 0; i < Parameters.serial_refinements; ++i)
       mesh.UniformRefinement();
     //Make Parallel Mesh
-    //ParMesh pmesh(MPI_COMM_WORLD, mesh);
+    ParMesh pmesh(MPI_COMM_WORLD, mesh);
     mesh.Clear();
     //Refine mesh (parallel)
     for (int ii = 0; ii < Parameters.parallel_refinements; ii++)
       pmesh.UniformRefinement();
-  } else {
+    return pmesh;
+    } else {
     //Read the input mesh
-    std::ifstream in;
     std::ostringstream oss;
-    oss << std::setw(10) << std::setfill('0') << Parameters.pid;
+    oss << std::setw(5) << std::setfill('0') << Parameters.pid;
     std::string n_mesh = "Initial/pmesh_"+oss.str()+".msh";
-    in.open(n_mesh.c_str(),std::ios::in);
-    pmesh.Load(in,1,0); 
-    in.close();
-  }
-  return pmesh;
+    std::ifstream mesh_ifs(n_mesh.c_str());
+    ParMesh pmesh(MPI_COMM_WORLD, mesh_ifs);
+    return pmesh;
+    }
 }
