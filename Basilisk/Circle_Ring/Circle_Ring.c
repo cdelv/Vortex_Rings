b464ca0bc650b4205d91bfdf09aeaf7907158033
Carlos <cdelv@unal.edu.co>
2022-12-15 22:03:08 -0500

    config file

--- Basilisk/Circle_Ring/Circle_Ring.c
+++ Basilisk/Circle_Ring/Circle_Ring.c
@@ -15,12 +15,6 @@
 #include "utils.h"
 
 /*
-  To limit simulation time and to restart simulation if possible
-
-#include "maxruntime.h"
-# define MAXTIME 0.05*/
-
-/*
   This include is for Paraview visualization. We took it from Sander Sandbox.
   Thank you very much for your help, Maximilian Sander.
   See http://basilisk.fr/sandbox/sander/output_htg.h.
@@ -35,39 +29,34 @@
 */
 #include "output_htg.h"
 
-/*
-  Time Variables:
-  - tend: Finalization time.
-*/
-double tend = 20. + 0.1;
+struct Config {
+  // Finalization time.
+  double tmax;
 
-/*
-  Fluid Parameters: CHANGE FOR VISCOSITY
-  - Re: Reynolds number
-*/
-double Re = 1750.0;
+  // Box dimensions.
+  double L;
 
-/*
-  Vortex Ring Parameters:
-  -Gamma: Magnitude of the vortex ring.
-  -aa: Sigma of the gaussian (thickness of the ring).
-  -RR: Vortex radius.
-  -LL: Initial position in the z-direction.
-  -ttol: Tolerance for the Laplace solver.
-*/
-double Gamma = 1.0;
-double aa = 0.2;
-double RR = 1.5;
-double LL = 10.0;
-double ttol = 1.0e-8;
+  // Ring Parameters, radius, Initial position in the z-direction,
+  //Sigma of the gaussian (thickness of the ring), Magnitude of the vortex, and
+  // Tolerance for the Laplace solver.
+  // R=1 and Gamma=1 always.
+  double R, Z0, a, Gamma, tol;
 
-/*
-  AMR variables:
-  - maxlevel: maximum refinement depth in the tree.
-  - ue: threshold for the refinement.
-*/
-int maxlevel = 8;
-double ue = 0.008;
+  // Fluid parameters
+  double Re, viscosity;
+
+  // AMR parameters
+  int max_level, initial_level;
+  double threshold;
+
+  // File saving parameters
+  char *path;
+} conf;
+
+double save_dt = 0.1;
+
+// Initialize the values of the Config struct
+void init_values(int argc, char *argv[]);
 
 /*
   For vorticity initial conditions.
@@ -98,7 +87,7 @@ u.n[back]   = neumann(0.0);
   Pressure Boundary Conditions
 */
 p[front]  = neumann(0.0);
-p[back]  =  neumann(0.0);
+p[back]   =  neumann(0.0);
 p[top]    = dirichlet(0.0);
 p[bottom] = dirichlet(0.0);
 p[right]  = dirichlet(0.0);
@@ -121,40 +110,42 @@ vz0[back] = dirichlet(0.0);
 */
 void curl(const vector v, vector curl);
 
-int main() {
-    init_grid (64);
-    size(32.0);
-    X0 = Y0 = Z0 = -L0 / 2;
-    const face vector muc[] = {1. / Re, 1. / Re, 1. / Re};
-    mu = muc;
-    run();
+int main(int argc, char *argv[]) {
+  init_values(argc, argv);
+  init_grid ((int)pow(2,conf.initial_level));
+  size(conf.L);
+  X0 = Y0 = Z0 = -L0 / 2;
+  const face vector muc[] = {conf.viscosity, conf.viscosity, conf.viscosity};
+  mu = muc;
+  run();
 }
 
 /*
   Initial Condition: we initialize the velocity from a vorticity field.
 */
 event init(t = 0.0) {
-    // Restart simulation from previus run
-    //if (!restore (file = "restart")){
-        // Initialize the forcing terms of the Poisson equation
-        foreach () {
-            bx[] = curl_w0_x(x, y, z);
-            by[] = curl_w0_y(x, y, z);
-            bz[] = curl_w0_z(x, y, z);
-        }
+  // Restart simulation from previus run
+  //if (!restore (file = "restart")){
+  // Initialize the forcing terms of the Poisson equation
+  foreach () {
+    bx[] = curl_w0_x(x, y, z);
+    by[] = curl_w0_y(x, y, z);
+    bz[] = curl_w0_z(x, y, z);
+  }
 
-        // Solve Poisson equation
-        poisson (vx0, bx, tolerance = ttol);
-        poisson (vy0, by, tolerance = ttol);
-        poisson (vz0, bz, tolerance = ttol);
+  // Solve Poisson equation
+  // \nabla( \alpha \nabla(a)) - \lamba a = b
+  poisson (vx0, bx, tolerance = conf.tol);
+  poisson (vy0, by, tolerance = conf.tol);
+  poisson (vz0, bz, tolerance = conf.tol);
 
-        // Initialize velocity
-        foreach () {
-            u.x[] = vx0[];
-            u.y[] = vy0[];
-            u.z[] = vz0[];
-        }
-    //}
+  // Initialize velocity
+  foreach () {
+    u.x[] = vx0[];
+    u.y[] = vy0[];
+    u.z[] = vz0[];
+  }
+  //}
 }
 
 /*
@@ -162,92 +153,124 @@ event init(t = 0.0) {
   - We use the velocity as the criteria for refinement.
 */
 event adapt (i++) {
-    astats s = adapt_wavelet ((scalar*) {u}, (double[]) {1.6 * ue, ue, ue}, maxlevel);
-    fprintf (stderr, "# Time %3f step %d -> refined %d cells, coarsened %d cells\n", t, i, s.nf, s.nc);
+  astats s = adapt_wavelet ((scalar*) {u}, (double[]) {conf.threshold , conf.threshold, conf.threshold}, conf.max_level);
+  fprintf (stderr, "# Time %3f step %d -> refined %d cells, coarsened %d cells\n", t, i, s.nf, s.nc);
 }
 
 /*
   Paraview Output:
   - W_vec: Vectorial vorticity field (W)
-  - W_mag: Magnitude of the vorticity 
+  - W_mag: Magnitude of the vorticity
   - W_r: W*r where r = sqrt(x^2 + y^2)
   - W_r2: W*r^2 where r = sqrt(x^2 + y^2)
   - W_Z: W*(z-L) where L is the initial position of the ring
   - W_Z2: W*(z-L)^2 where L is the initial position of the ring
   - PID: Process ID of the CPU in charge of that cell
   - LEVEL: Level of refinement. The number of cells is 2^LEVEL
+  CHECK FOR FILE EXISTANCE
 */
-event snapshots (t += 0.2) {
-    vector W_vec[], W_r[], W_r2[], W_Z[], W_Z2[];
-    scalar W_mag[], PID[], LEVEL[];
+event snapshots (t += save_dt) {
+  vector W_vec[], W_r[], W_r2[], W_Z[], W_Z2[];
+  scalar W_mag[], PID[], LEVEL[];
 
-    curl(u, W_vec);
+  curl(u, W_vec);
 
-    foreach () {
-        double r = hypot(x, y);
-        W_mag[] = norm(W_vec);
-        PID[] = pid();
-        LEVEL[] = level;
-        foreach_dimension() {
-            W_r.x[] = r * W_vec.x[];
-            W_r2.x[] = pow(r , 2) * W_vec.x[];
-            W_Z.x[] = (z - LL) * W_vec.x[];
-            W_Z2.x[] = pow(z - LL, 2) * W_vec.x[];
-        }
+  foreach () {
+    double r = hypot(x, y);
+    W_mag[] = norm(W_vec);
+    PID[] = pid();
+    LEVEL[] = level;
+    foreach_dimension() {
+      W_r.x[] = r * W_vec.x[];
+      W_r2.x[] = pow(r , 2) * W_vec.x[];
+      W_Z.x[] = (z - conf.Z0) * W_vec.x[];
+      W_Z2.x[] = pow(z - conf.Z0, 2) * W_vec.x[];
     }
+  }
 
-    // Paraview output
-    char path[] = "htg"; // no slash at the end!!
-    char prefix[80];
-    sprintf(prefix, "data_%03d_%06d", (int) t, i);
-    output_htg((scalar *) {W_mag, PID, LEVEL}, (vector *) {uf, W_vec, W_r, W_r2, W_Z, W_Z2}, path, prefix, i, t);
+  // Paraview output
+  char path[] = "htg"; // no slash at the end!!
+  char prefix[80];
+  sprintf(prefix, "data_%03d_%06d", (int) t, i);
+  output_htg((scalar *) {W_mag, PID, LEVEL}, (vector *) {uf, W_vec, W_r, W_r2, W_Z, W_Z2}, conf.path, prefix, i, t);
 }
 
-event stop (t = tend);
+event stop (t = conf.tmax);
+
+void init_values(int argc, char *argv[]) {
+  char *ptr;
+  conf.tmax = strtod(argv[1], &ptr);
+  conf.L = strtod(argv[2], &ptr);
+  conf.R = 1.0;
+  conf.Z0 = strtod(argv[3], &ptr);
+  conf.a = strtod(argv[4], &ptr);
+  conf.Gamma = 1.0;
+  conf.tol = strtod(argv[5], &ptr);
+  conf.Re = strtod(argv[6], &ptr);
+  conf.viscosity = 1.0 / conf.Re;
+  conf.initial_level = strtod(argv[7], &ptr);
+  conf.max_level = strtod(argv[8], &ptr);
+  conf.threshold = strtod(argv[9], &ptr);
+  save_dt = strtod(argv[10], &ptr);
+  conf.path = argv[11];
+  if (pid() == 0) {
+    printf("tmax = %g \n", conf.tmax);
+    printf("L = %g \n", conf.L);
+    printf("Z0 = %g \n", conf.Z0);
+    printf("a = %g \n", conf.a);
+    printf("tol = %g \n", conf.tol);
+    printf("Re = %g \n", conf.Re);
+    printf("initial_level = %d \n", conf.initial_level);
+    printf("max_level = %d \n", conf.max_level);
+    printf("threshold = %g \n", conf.threshold);
+    printf("save_dt = %g \n", save_dt);
+    printf("path = %s \n", conf.path);
+  }
+}
 
 double W(double r, double z) {
-    double scale = Gamma / (M_PI * pow(aa, 2));
-    double T1 = pow(r - RR, 2);
-    double T2 = pow(z - LL, 2);
-    return scale * exp(-(T1 + T2) / pow(aa, 2));
+  double scale = conf.Gamma / (M_PI * pow(conf.a, 2));
+  double T1 = pow(r - conf.R, 2);
+  double T2 = pow(z - conf.Z0, 2);
+  return scale * exp(-(T1 + T2) / pow(conf.a, 2));
 }
 
 double curl_w0_x(double x, double y, double z) {
-    double r = hypot(x, y) + 1.0e-6;
-    double T1 = 2.0 * (z - LL) / (r * pow(aa, 2));
-    return W(r, z) * T1 * x;
+  double r = hypot(x, y) + 1.0e-6;
+  double T1 = 2.0 * (z - conf.Z0) / (r * pow(conf.a, 2));
+  return W(r, z) * T1 * x;
 }
 
 double curl_w0_y(double x, double y, double z) {
-    double r = hypot(x, y) + 1.0e-6;
-    double T1 = 2.0 * (z - LL) / (r * pow(aa, 2));
-    return W(r, z) * T1 * y;
+  double r = hypot(x, y) + 1.0e-6;
+  double T1 = 2.0 * (z - conf.Z0) / (r * pow(conf.a, 2));
+  return W(r, z) * T1 * y;
 }
 
 double curl_w0_z(double x, double y, double z) {
-    double r = hypot(x, y) + 1.0e-6;
-    double T1 = 1.0 / r;
-    double T2 = z * (r - RR) / pow(aa, 2);
-    return W(r, z) * (T1 - T2);
+  double r = hypot(x, y) + 1.0e-6;
+  double T1 = 1.0 / r;
+  double T2 = z * (r - conf.R) / pow(conf.a, 2);
+  return W(r, z) * (T1 - T2);
 }
 
 void curl(const vector v, vector curl) {
-    vector dvx[], dvy[], dvz[];
-    scalar vx[], vy[], vz[];
+  vector dvx[], dvy[], dvz[];
+  scalar vx[], vy[], vz[];
 
-    foreach () {
-        vx[] = v.x[];
-        vy[] = v.y[];
-        vz[] = v.z[];
-    }
+  foreach () {
+    vx[] = v.x[];
+    vy[] = v.y[];
+    vz[] = v.z[];
+  }
 
-    gradients ({vx}, {dvx});
-    gradients ({vy}, {dvy});
-    gradients ({vz}, {dvz});
+  gradients ({vx}, {dvx});
+  gradients ({vy}, {dvy});
+  gradients ({vz}, {dvz});
 
-    foreach () {
-        curl.x[] = dvz.y[] - dvy.z[];
-        curl.y[] = dvx.z[] - dvz.x[];
-        curl.z[] = dvy.x[] - dvx.y[];
-    }
+  foreach () {
+    curl.x[] = dvz.y[] - dvy.z[];
+    curl.y[] = dvx.z[] - dvz.x[];
+    curl.z[] = dvy.x[] - dvx.y[];
+  }
 }
---
+++ Basilisk/Circle_Ring/config.mk
@@ -0,0 +1,24 @@
+
+# Simulation Time
+tmax = 20.0
+
+# Simulation Domain
+L = 32.0
+
+# Ring Parameters
+Z0 = 0.0
+a = 0.2
+tol = 1e-8
+ 
+# Fluid parameters
+Re = 500
+
+# AMR parameters
+initial_level = 6
+max_level = 8
+threshold = 0.008
+
+# Save parameters
+save_dt = 0.2
+path = htg
+
