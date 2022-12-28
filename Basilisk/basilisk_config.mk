3284c15a91e5d4a28a4b5d4599c9cddd42833719
jujimenezp <jujimenezp@unal.edu.co>
2022-12-28 17:58:45 -0500

    Makefile improved

--- Basilisk/basilisk_config.mk
+++ Basilisk/basilisk_config.mk
@@ -1,4 +1,5 @@
 
 BASILISK = /home/wind/Documents/Vortex_Rings/Basilisk/basilisk/src
 Paraview_Out = /home/wind/Documents/Vortex_Rings/Basilisk/Paraview_Out
+SHARED_FOLDER = /media/sf_Shared_Folder/vortex_rings/
 PROCCESORS = 4
